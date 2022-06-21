65966942fd0ac68e565ae1b4e8f23472fe6b5c92
Jimmy Wu <jimmy2822@gmail.com>
2022-06-21 23:46:14 +0800

    * Implement rake db:seed * Add sing in button on navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create(email: 'test@gmail.com', password: '123456')
+
+HouseScraper.new.crawl!
