fdba396470d3d031d50531f0e5d2430d22d78897
Jimmy Wu <jimmy2822@gmail.com>
2022-06-27 19:53:22 +0800

    * Implement favorites page login check

--- app/javascript/routes/favorites.js
+++ app/javascript/routes/favorites.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import useEffect from 'react';
 import Navbar from '../components/layouts/navbar.jsx';
+import { useNavigate } from 'react-router-dom';
+import { useQueryClient } from 'react-query';
 
 const Favorites = () => {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const accessToken = queryClient.getQueryData('accessToken');
+
+  if (accessToken === undefined) {
+    return (
+      <main className='container mx-auto px-5 md:px-60'>
+        <Navbar />
+        <div>Please sign in to get your favorites.</div>
+      </main>
+    )
+  }
+
   return (
     <main className='container mx-auto px-5 md:px-60'>
       <Navbar />
-      <div>Favorite Page</div>
+      <div className="my-5">
+        <div className="text-center font-bold">My Favorites</div>
+      </div>
     </main>
   )
 };
