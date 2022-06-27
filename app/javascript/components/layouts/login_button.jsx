89f4c9cdb6ccb458088ec5e636c4023ce777bc8d
Jimmy Wu <jimmy2822@gmail.com>
2022-06-27 17:38:20 +0800

    * Implement login button with token checking logic

--- app/javascript/components/layouts/login_button.jsx
+++ app/javascript/components/layouts/login_button.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import {
+  Link,
+  useNavigate
+} from 'react-router-dom';
+import { useQueryClient } from 'react-query';
 
 const LoginButton = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const accessToken = queryClient.getQueryData('accessToken');
+
+  const clearAccessToken = () => {
+    queryClient.setQueryData('accessToken', undefined);
+    navigate('/');
+  };
+
+  const signInButton = <Link to='/users/sign_in'>Sign In</Link>
+  const signOutButton = <button onClick={clearAccessToken}>Sign Out</button>
+
+
   return (
-    displayButton()
+    <div>
+      { accessToken === undefined ? signInButton : signOutButton }
+    </div>
   )
-};
-
-const displayButton = () => {
-  const singInButton = <a href='/users/sign_in'>Sign In</a>
-  const singOutButton = <a href='/users/sign_out'>Sign Out</a>
-
-  return window.user_id === null ? singInButton : singOutButton
 };
 
 export default LoginButton;
--- app/javascript/routes/users/sign_in.js
+++ app/javascript/routes/users/sign_in.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import {
+  useQuery,
+  useQueryClient,
+  useMutation,
+} from 'react-query';
 
 import NavBar from '../../components/layouts/navbar.jsx'
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const queryClient = useQueryClient();
+  const mutation = useMutation(getAuthToken, {
+    onSuccess: (data) => {
+      queryClient.setQueryData(['accessToken'], data.access_token);
+      if (data.error === undefined) navigate('/');
+    }
+  });
 
   return (
     <div>
       <div className='text-lg my-5 text-center'>Sign In</div>
-      <form className="flex flex-col px-20 py-5 border-2" onSubmit={handleSubmit(onSubmit)}>
+      <form className="flex flex-col px-20 py-5 border-2" onSubmit={handleSubmit(mutation.mutate)}>
         <div className="flex flex-col">
           <div className="font-bold mb-3">Email</div>
           <input className="border-b-2 text-gray-400" {...register("email", { required: true })} />
@@ -21,10 +34,23 @@ const SignIn = () => {
           <input type='password' className="border-b-2 text-gray-400" {...register("password", { required: true })} />
           {errors.password && <span className="text-red-400">This field is required</span>}
         </div>
-        <input type="submit" className="mt-5 border-2 border-radious-2 hover:bg-blue-400"/>
+        <input type="submit" className="mt-5 border-2 border-radious-2 bg-blue-400 hover:bg-blue-600"/>
+        {mutation.status === 'success' ? <span className='text-red-600'>Login failed</span> : ''}
       </form>
     </div>
   )
 }
+
+const getAuthToken = async (data) => {
+  const formData = new FormData();
+  formData.append('grant_type', 'password');
+  formData.append('email', data.email);
+  formData.append('password', data.password);
+
+  return await fetch('/oauth/token', {
+    method: 'POST',
+    body: formData
+  }).then(data => data.json());
+};
 
 export default SignIn;
