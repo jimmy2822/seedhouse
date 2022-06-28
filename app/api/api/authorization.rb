81a92b5ce0aa29593ed07e9de7f0eb754cc6dc61
Jimmy Wu <jimmy2822@gmail.com>
2022-06-28 22:55:27 +0800

    * Implement api v1 favorites

--- app/api/api/authorization.rb
+++ app/api/api/authorization.rb
@@ -2,8 +2,5 @@
 
 module API
   module Authorization
-    def current_user
-      env['warden'].authenticate(scope: :user)
-    end
   end
 end
---
+++ app/api/v1/favorites.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module V1
+  class Favorites < Grape::API
+    helpers API::HasResponse
+    helpers API::Authorization
+
+    helpers do
+      params :pagination do
+        optional :page, type: Integer
+      end
+    end
+
+    desc 'Properties data'
+    params do
+      use :pagination, default_page: 1
+    end
+    get '/v1/favorites' do
+      query_result = User
+                     .first
+                     .favorite_properties
+                     .ransack
+                     .result
+                     .order(amount_in_cent: :asc)
+                     .page(params[:page])
+                     .per(8)
+      response_data = {
+        items: query_result,
+        pagination: {
+          total_pages: query_result.total_pages,
+          current_page: query_result.current_page
+        }
+      }
+      respond(data: response_data)
+    end
+  end
+end
