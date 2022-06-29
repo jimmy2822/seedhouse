b42524e5d58022433219cbaba0076e45ee7df67a
Jimmy Wu <jimmy2822@gmail.com>
2022-06-30 00:41:44 +0800

    * Implment API endpoint post /v1/favorites * Implment API endpoint delete /v1/favorites

--- app/api/v1/favorites.rb
+++ app/api/v1/favorites.rb
@@ -5,18 +5,10 @@ module V1
     helpers API::HasResponse
     helpers API::Authorization
 
-    helpers do
-      params :pagination do
-        optional :page, type: Integer
-      end
-    end
-
     before { authenticate! }
 
     desc 'Get favorites by user'
-    params do
-      use :pagination, default_page: 1
-    end
+
     get '/v1/favorites' do
       query_result = current_user
                      .favorite_properties
@@ -32,6 +24,31 @@ module V1
           current_page: query_result.current_page
         }
       }
+
+      respond(data: response_data)
+    end
+
+    post '/v1/favorites' do
+      favorite = current_user.favorites.find_or_create_by(property_id: params[:property_id])
+      user_favorite_property_ids = current_user.favorites.pluck(:property_id)
+
+      response_data = {
+        favorite_property_ids: user_favorite_property_ids,
+        message: favorite ? 'success' : 'failed to create'
+      }
+
+      respond(data: response_data)
+    end
+
+    delete '/v1/favorites' do
+      destroyed_list = current_user.favorites.destroy_by(property_id: params[:property_id])
+      user_favorite_property_ids = current_user.favorites.pluck(:property_id)
+
+      response_data = {
+        favorite_property_ids: user_favorite_property_ids,
+        message: destroyed_list.present? ? 'success' : 'favorite dose not exist'
+      }
+
       respond(data: response_data)
     end
   end
