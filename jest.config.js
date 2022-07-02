5e5d4a85a18eb188432c77074379843b0fb51424
Jimmy Wu <jimmy2822@gmail.com>
2022-07-02 21:04:02 +0800

    * Add esbuild jest configs for tests

--- jest.config.js
+++ jest.config.js
@@ -174,7 +174,15 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(js|jsx)?$": "esbuild-jest"
+    "^.+\\.(js|jsx)?$": [
+      "esbuild-jest",
+      { 
+        sourcemap: true,
+        loaders: {
+          '.test.js': 'jsx'
+        }
+      } 
+    ]
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
