eec88db59116125243ac57a111b6076f0faa24d4
Jimmy Wu <jimmy2822@gmail.com>
2022-07-15 12:47:04 +0800

    * Fix admin page login by device CSRF token issue

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     skip_controllers :authorizations, :applications, :authorized_applications
   end
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   root to: "properties#index"
 end
